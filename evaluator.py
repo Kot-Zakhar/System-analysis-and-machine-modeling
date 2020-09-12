248c38b70680ca6e69c3ff62f0d95bbb953fd7c3
Alexei Bobko <abvbobko@gmail.com>
2020-09-12 22:26:35 +0300

    Add checking by indirect signs

--- evaluator.py
+++ evaluator.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from math import pi
 
 
 def hist(x, bins=20):
@@ -18,3 +19,16 @@ def dispersion(x):
 
 def standard_deviation(x):
     return dispersion(x)**(1/2)
+
+
+def indirect_signs_check(x, error):
+    n = len(x) if (len(x) % 2 == 0) else (len(x) - 1)
+    k = 0
+    for i in range(0, n-1, 2):
+        if x[i]**2 + x[i+1]**2 < 1:
+            k += 1
+
+    if 2*k/n - pi/4 < error:
+        return True
+    return False
+
--- main.py
+++ main.py
@@ -8,8 +8,11 @@ if __name__ == '__main__':
     # R0 = int(input('R0: '))
     # x = uniform_distribution.lehmer(a=3, m=5, R0=1)
     x = uniform_distribution.lehmer(a=5, m=4096, R0=2451)
+    # x = uniform_distribution.lehmer(a=9, m=113, R0=42)
     print(len(x))
     print('m:', evaluator.math_expectation(x))
     print('D:', evaluator.dispersion(x))
     print('σ:', evaluator.standard_deviation(x))
     evaluator.hist(x, bins=20)
+    print(evaluator.indirect_signs_check(x, 0.01))
+
