a22bbfd03f2696960b463774971877f63dcbfa1d
Alexei Bobko <abvbobko@gmail.com>
2020-09-12 18:14:07 +0300

    Implement Lehmer algorithm

---
+++ generator/uniform_distribution.py
@@ -0,0 +1,21 @@
+
+
+def lehmer(a, m, R0):
+
+    # m > a > 0
+    R_prev = R0
+    x = []
+    while True:
+        R_curr = a * R_prev % m
+        x_i = R_curr / m
+        R_prev = R_curr
+        if x_i in x:
+            print(x_i)
+            break
+        else:
+            x.append(x_i)
+    return x
+
+
+if __name__ == '__main__':
+    print(lehmer(a=3, m=5, R0=1))
---
+++ main.py
@@ -0,0 +1,6 @@
+
+
+if __name__ == '__main__':
+    a = int(input('a: '))
+    m = int(input('m: '))
+    R0 = int(input('R0: '))
