409176764b4c50935fb405b671dad49202b9020e
Alexei Bobko <abvbobko@gmail.com>
2020-09-19 13:16:15 +0300

    Add normal dist func

--- generator.py
+++ generator.py
@@ -1,5 +1,3 @@
-
-
 def lehmer(a, m, R0):
 
     # m > a > 0
@@ -11,13 +9,31 @@ def lehmer(a, m, R0):
         x_i = R_curr / m
         R_prev = R_curr
         if x_i in x_set:
-            period = len(x) - x.index(x_i)
+            # period = len(x) - x.index(x_i)
             break
         else:
             x_set.add(x_i)
             x.append(x_i)
-    return x, period
+    return x  # , period
+
+
+def uniform_distribution(a, b, **args):
+    def uniform_generation_func(a, b, R):
+        return a + (b - a)*R
+    R = lehmer(a=args['A'], m=args['M'], R0=args['R0'])
+    return [uniform_generation_func(a, b, r) for r in R]
+
+
+def normal_distribution(m, sigma, n=6, **args):
+    def normal_generation_func(m, sigma, Ri_sum, n):
+        return m + sigma * (Ri_sum - n/2) * ((12/n)**(1/2))
+    R = lehmer(a=args['A'], m=args['M'], R0=args['R0'])
+    return [normal_generation_func(m, sigma, sum(R[i-n: i]), n) for i in range(n, len(R), n)]
 
 
 if __name__ == '__main__':
-    print(lehmer(a=3, m=5, R0=1))
+    # A 39999
+    # m: 59999
+    # R0: 19999
+    # print(uniform_distribution(1, 10, A=39999, M=59999, R0=19999))
+    print(normal_distribution(3, 3, A=39999, M=59999, R0=19999))
--- main.py
+++ main.py
@@ -7,17 +7,11 @@ def get_statistics(x, P):
     M = evaluator.math_expectation(x)
     D = evaluator.dispersion(x)
     sigma = evaluator.standard_deviation(x)
-    k2_on_4 = evaluator.indirect_signs_check(x)
-    L = len(x)
 
     return "\n".join([
         f'M: {M}',
         f'D: {D}',
-        f'σ: {sigma}',
-        f'2K/4: {k2_on_4}',
-        f'pi/4: {pi/4}',
-        f'L: {L}',
-        f'P: {P}'
+        f'σ: {sigma}'
     ])
 
 
@@ -28,19 +22,44 @@ def process(a, m, R0):
     print(get_statistics(x, period))
     evaluator.hist(x, bins=20)
 
-# 39999
-# m: 59999
-# R0: 19999
+
+def get_distribution_dict():
+    return {
+        "uniform": None,
+        "normal": None,
+        "exponential": None,
+        "gamma": None,
+        "triangular": None,
+        "simpson": None
+    }
+
+
+def main():
+    dist_dict = get_distribution_dict()
+    dists = list(dist_dict.keys())
+    while True:
+        for i in range(len(dists)):
+            print(f"{i+1}. {dists[i]};")
+        exit_option = len(dists) + 1
+        print("----------------")
+        print(f"{exit_option}. exit.")
+        option = int(input("Select the distribution: "))
+        if option == exit_option:
+            break
+
+
+
 if __name__ == '__main__':
-    a = int(input('a: '))
-    m = int(input('m: '))
-    R0 = int(input('R0: '))
-    # a, m, R0 = 3, 5, 1
-    process(a=a, m=m, R0=R0)
-    print()
-    a, m, R0 = evaluator.find_parameters()
-    print(f'a: {a}\nm: {m}\nR0: {R0}')
-    process(a=a, m=m, R0=R0)
+    main()
+
+
+    # a = int(input('a: '))
+    # m = int(input('m: '))
+    # R0 = int(input('R0: '))
+    # # a, m, R0 = 3, 5, 1
+    # process(a=a, m=m, R0=R0)
+
+
 
 
 
